8c081c0740540f932793ea2180dabba67d405b92
Nathan Workman <nathancworkman@gmail.com>
2018-04-09 19:59:09 -0400

    Revert "Cleanup UI"

--- seeker/gulpfile.js
+++ seeker/gulpfile.js
@@ -40,8 +40,7 @@ gulp.task('css', function() {
 // Scripts
 gulp.task('js', function() {
   return gulp.src([
-    '../node_modules/jquery/dist/jquery.min.js',
-    '../node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+    src + 'js/lib/**.js',
     src + 'js/**.js'
   ])
   .pipe($.plumber({errorHandler: errorAlert}))
